4bfa924562f56214cc6eb4a1aef6045c46c8d928
TrashBin <96432924+Trashbin7@users.noreply.github.com>
2023-09-24 20:04:34 +0300

    Update project.py

--- project.py
+++ project.py
@@ -1,18 +1,18 @@
 import random
 while True:
-    my_answer=input("choose:rock,paperor scissors")
+    my_answer=input("choose between rock, paper or scissors")
     my_answer=my_answer.lower()
     if my_answer=="quit":
         break
     if my_answer !="rock" and my_answer!="paper" and my_answer!="scissors":
-        print("please choose a correct answer")
+        print("please choose between rock, paper, scissors")
         continue
     computer_answer=random.choice(["rock","paper","scissors"])
     if my_answer==computer_answer:
         print("you tied")
         continue
     elif my_answer=="paper" and computer_answer=="rock":
-        print("you won!")
+        print("you win!")
     elif my_answer=="rock" and computer_answer=="scissors":
         print("you win!")
     elif my_answer=="scissors" and computer_answer=="paper":
